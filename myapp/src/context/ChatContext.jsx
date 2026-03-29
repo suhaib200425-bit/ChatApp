e34ee5720eb38a416906dcfb7a9138bbe2ecd613
Suhaib2006 <suhaibkoppath66@gmail.com>
2026-03-29 21:59:23 +0530

    Api working alert

--- myapp/src/context/ChatContext.jsx
+++ myapp/src/context/ChatContext.jsx
@@ -1,4 +1,6 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { BACKENDURL } from "../assets/assets";
+import axios from 'axios'
 
  const ChatContext = createContext();
 
@@ -6,6 +8,15 @@ export const ChatContextProvider = ({ children }) => {
 
   const [user, setUser] = useState({});
 
+  useEffect(()=>{
+   async function apiWorking(){
+     const response= await axios.get(BACKENDURL)
+     alert(response.data)
+     console.log(response.data);
+     
+   }
+   apiWorking()
+  },[])
   return (
     <ChatContext.Provider value={{ user, setUser }}>
       {children}
